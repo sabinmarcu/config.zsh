3b9f7463417a4c5271127d16287ffcdda8fd6f00
Sabin Marcu <sabin.marcu@hootsuite.com>
2023-01-31 14:03:30 +0200

    feat: parameterize volta global versions

--- config/post/volta.zsh
+++ config/post/volta.zsh
@@ -1,10 +1,10 @@
 if command -v volta &> /dev/null; then
   if $(shouldUpdate); then
     ZDS=$0 debug Installing nodejs
-    volta install node@latest
+    volta install node@${VOLTA_VERSION_NODE:-latest}
     
     ZDS=$0 debug Installind yarn
-    volta install yarn@latest
+    volta install yarn@${VOLTA_VERSION_YARN:-latest}
 
     ZDS=$0 debug Setting up volta
     volta setup
