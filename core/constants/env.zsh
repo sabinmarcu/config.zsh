8e89db6407ef0abcb0429d81d75ec7a1d0f13c51
Sabin Marcu <sabinmarcu@gmail.com>
2023-05-18 01:00:50 +0300

    feat: add ZSH_LOCAL and XDG_CONFIG_HOME to env

--- core/constants/env.zsh
+++ core/constants/env.zsh
@@ -1,2 +1,4 @@
 export ANTIDOTE_HOME=${HOME}/.antidote
 export ZSH_CONFIG=${ZSH_CONFIG:-"$HOME/.zshrc"}
+export ZSH_LOCAL=${ZSH_LOCAL:-"$ZSH_CONFIG.local"}
+export XDG_CONFIG_HOME=${XDG_CONFIG_HOME:-"$HOME/.config"}
