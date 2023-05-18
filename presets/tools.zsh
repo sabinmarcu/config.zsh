e8e4c353d2f0ca20652b7b9a2051ee5ec8634b7d
Sabin Marcu <sabinmarcu@gmail.com>
2023-05-18 07:21:13 +0300

    feat: add yabai and skhd to macos tools

--- presets/tools.zsh
+++ presets/tools.zsh
@@ -16,3 +16,30 @@ if command -v pacman &> /dev/null; then
   ZDS=$0 debug "Detected ArchLinux. Adding extra tools"
   tools+=(xsel xsel)
 fi
+
+if [ $ZSH_PLATFORM = 'macos' ]; then
+  tools+=(
+    yabai koekeishiya/formulae/yabai
+    skhd koekeishiya/formulae/skhd
+  )
+  function tile {
+    case $1 in
+      start)
+        info "Starting tiling"
+        yabai --start-service
+        skhd --start-service
+        ;;
+      stop)
+        warn "Stopping tiling"
+        yabai --stop-service
+        skhd --stop-service
+        ;;
+      *)
+        if result=$(launchctl list | grep yabai); then
+          tile stop
+        else
+          tile start
+        fi
+    esac
+  }
+fi
