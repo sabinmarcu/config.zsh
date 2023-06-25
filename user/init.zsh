4c93f70bba6a55a1487eadc6db1ef76db74ffd51
Sabin Marcu <sabinmarcu@gmail.com>
2023-06-25 22:13:24 +0300

    feat: add tmux init with opt-out

--- user/init.zsh
+++ user/init.zsh
@@ -1,3 +1,4 @@
 import user.config
 import user.gitflip
 import user.tile
+import user.tmux
---
+++ user/tmux.zsh
@@ -0,0 +1,11 @@
+ZDS=$0 debug "Attempting TMUX init"
+if [ -z $ZSH_NO_TMUX ]; then 
+  ZDS=$0 debug "TMUX opt-out not found"
+  if [ -z $NVIM ]; then 
+    ZDS=$0 debug "Not in NVIM"
+    if [ -z $TMUX ]; then
+      ZDS=$0 debug "Also not in a session"
+      tmux attach || tmux
+    fi
+  fi
+fi
