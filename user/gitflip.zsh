8ee7f57084c7cc56a2295cc11afb90bd4cfbfd94
Sabin Marcu <sabinmarcu@gmail.com>
2023-06-26 19:23:56 +0300

    fix: fix ggflip to work with remotes without .git extensions

--- user/gitflip.zsh
+++ user/gitflip.zsh
@@ -1,9 +1,14 @@
+local ds=$(debugScope $0)
+
 function ggflip() {
   local currentOrigin=$(git remote -v | grep origin | awk '$1=$1' | cut -d " " -f2 | uniq)
-  local shortHand=$(echo $currentOrigin | sed -E 's/.*(\/|:)([^\/]+\/[^\/]+).git$/\2/')
-  local host=$(echo $currentOrigin | sed -E 's/(git@|https:\/\/)([^\/:]+)[\/:].*/\2/')
+  local shortHand=$(echo $currentOrigin | sed -E 's/.*(\/|:)([^\/]+\/[^\/]+)(\.git)?$/\2/' | sed -E 's/.git$//')
+  local host=$(echo $currentOrigin | sed -E 's/(git@|https?:\/\/)([^\/:]+)[\/:].*/\2/')
+  ZDS=$ds debug "currentOrigin=${currentOrigin}"
+  ZDS=$ds debug "shortHand=${shortHand}"
+  ZDS=$ds debug "host=${host}"
   local newOrigin
-  if [ -n "$(echo $currentOrigin | grep https)" ]; then
+  if [ -n "$(echo $currentOrigin | grep http)" ]; then
     newOrigin="git@${host}:${shortHand}.git"
   else
     newOrigin="https://${host}/${shortHand}.git"
