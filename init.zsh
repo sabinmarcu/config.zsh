265bae850a6b77e76fe6f17bc13dff803ab352a5
Sabin Marcu <sabinmarcu@gmail.com>
2023-11-29 06:16:15 +0200

    fix: remove proto and dvm from root init script

--- init.zsh
+++ init.zsh
@@ -30,9 +30,3 @@ import user.*
 
 _cleanupImportCache
 _runCleanup
-export DVM_DIR="/Users/sabinmarcu/.dvm"
-export PATH="$DVM_DIR/bin:$PATH"
-
-# proto
-export PROTO_HOME="$HOME/.proto"
-export PATH="$PROTO_HOME/shims:$PROTO_HOME/bin:$PATH"
