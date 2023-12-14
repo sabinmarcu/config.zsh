cfc8e4a6ceb57ba7e504e6f256a0a5410a24f1c0
Sabin Marcu <sabinmarcu@gmail.com>
2023-12-14 14:28:43 +0100

    feat: add binary paths to proto setup

--- config/post/proto.zsh
+++ config/post/proto.zsh
@@ -3,7 +3,7 @@ if command -v proto &> /dev/null; then
     ZDS=$0 debug Installing nodejs
     proto install node ${PROTO_VERSION_NODE:-latest}
     
-    ZDS=$0 debug Installind yarn
+    ZDS=$0 debug Installing yarn
     proto install yarn ${PROTO_VERSION_YARN:-latest}
 
     ZDS=$0 debug Setting up proto
@@ -15,3 +15,31 @@ else
 fi
 
 
+# Go paths
+if ! [ -z $GOBIN ]; then
+  export PATH="$PATH:$GOBIN"
+  export PATH="$PATH:$GOBIN/bin"
+  export PATH="$PATH:$HOME/go/bin"
+fi
+
+if ! [ -z $GOPATH ]; then
+  export PATH="$PATH:$GOPATH/bin"
+fi
+
+# Node paths
+export PATH="$PATH:$HOME/.proto/tools/node/globals/bin"
+
+# Python (experimental)
+export PATH="$PATH:$HOME/.local/bin"
+
+# Bun
+export PATH="$PATH:$HOME/.bun/bin"
+
+# Deno
+if ! [ -z $DENO_INSTALL_ROOT ]; then
+  export PATH="$PATH:$DENO_INSTALL_ROOT/bin"
+fi
+if ! [ -z $DENO_HOME ]; then
+  export PATH="$PATH:$DENO_HOME/bin"
+fi
+export PATH="$PATH:$HOME/.deno/bin"
