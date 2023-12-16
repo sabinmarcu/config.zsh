c74c934bbb77d27f1b281d4bbbc26a1645958fd2
Sabin Marcu <sabinmarcu@gmail.com>
2023-12-16 17:50:02 +0100

    feat: add optional StageManager toggle in turn with tiling

--- user/tile.zsh
+++ user/tile.zsh
@@ -13,12 +13,24 @@ if command -v yabai &> /dev/null && command -v skhd &> /dev/null; then
       start)
         info "Starting tiling"
         yabai --start-service
+        ZDS=$0 debug "Starting yabai"
         skhd --start-service
+        ZDS=$0 debug "Starting skhd"
+        if command -v defaults &> /dev/null; then
+          ZDS=$0 debug "Stopping StageManager (macos)"
+          defaults write com.apple.WindowManager GloballyEnabled -bool false
+        fi
         ;;
       stop)
         warn "Stopping tiling"
+        ZDS=$0 debug "Stopping yabai"
         yabai --stop-service
+        ZDS=$0 debug "Stopping skhd"
         skhd --stop-service
+        if command -v defaults &> /dev/null; then
+          ZDS=$0 debug "Starting StageManager (macos)"
+          defaults write com.apple.WindowManager GloballyEnabled -bool true
+        fi
         ;;
       *)
         if result=$(launchctl list | grep yabai); then
