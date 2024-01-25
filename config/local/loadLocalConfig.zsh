b6afd0adeb383fe94c416fb90e9b56323655d69e
Sabin Marcu <sabinmarcu@gmail.com>
2024-01-25 17:24:40 +0200

    feat: improve local config detection

--- config/local/loadLocalConfig.zsh
+++ config/local/loadLocalConfig.zsh
@@ -4,7 +4,7 @@ function loadLocalConfig {
   local pre=${1:-""}
 
   ZDS=$ds debug "Looking for local configs (prefix: '$pre')"
-  if echo $HOME/${pre}.zshrc.*.local &> /dev/null; then
+  if [[ -n $HOME/${pre}.zshrc.*.local(#qN) ]]; then
     ZDS=$ds debug "Found local configs, loading now"
     for localRc in $HOME/${pre}.zshrc.*.local; do
       ZDS=$ds debug "Loading local config (at: $localRc)"
