5bdf768194c9a78527c605d3a3bd733cf1a441fa
Sabin Marcu <sabinmarcu@gmail.com>
2024-12-09 15:34:26 +0200

    fix: local config loading silence non existing prompts

--- config/local/loadLocalConfig.zsh
+++ config/local/loadLocalConfig.zsh
@@ -4,11 +4,11 @@ function loadLocalConfig {
   local pre=${1:-""}
 
   ZDS=$ds debug "Looking for local configs (prefix: '$pre')"
-  if [[ -n $HOME/${pre}.zshrc.local ]]; then
+  if [[ -n $HOME/${pre}.zshrc.local(#qN) ]] &> /dev/null; then
     ZDS=$ds debug "Found main local config, loading now"
     source $HOME/${pre}.zshrc.local
   fi
-  if [[ -n $HOME/${pre}.zshrc.*.local(#qN) ]]; then
+  if [[ -n $HOME/${pre}.zshrc.*.local(#qN) ]] &> /dev/null; then
     ZDS=$ds debug "Found local configs, loading now"
     for localRc in $HOME/${pre}.zshrc.*.local; do
       ZDS=$ds debug "Loading local config (at: $localRc)"
