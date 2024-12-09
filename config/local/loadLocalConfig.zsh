e51f0fe551f7f7e3931ce33576709bdc3851efec
Sabin Marcu <sabinmarcu@gmail.com>
2024-12-09 15:28:59 +0200

    fix: fix local config loading for main

--- config/local/loadLocalConfig.zsh
+++ config/local/loadLocalConfig.zsh
@@ -4,6 +4,10 @@ function loadLocalConfig {
   local pre=${1:-""}
 
   ZDS=$ds debug "Looking for local configs (prefix: '$pre')"
+  if [[ -n $HOME/${pre}.zshrc.local ]]; then
+    ZDS=$ds debug "Found main local config, loading now"
+    source $HOME/${pre}.zshrc.local
+  fi
   if [[ -n $HOME/${pre}.zshrc.*.local(#qN) ]]; then
     ZDS=$ds debug "Found local configs, loading now"
     for localRc in $HOME/${pre}.zshrc.*.local; do
