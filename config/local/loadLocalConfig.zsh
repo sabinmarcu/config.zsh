adc98de4e619776e1bb9a2e2d95a90a36157de47
Sabin Marcu <sabinmarcu@gmail.com>
2025-02-28 13:23:45 +0200

    fix: fix loadLocalConfig to not print out messages when files are not found

--- config/local/loadLocalConfig.zsh
+++ config/local/loadLocalConfig.zsh
@@ -4,15 +4,19 @@ function loadLocalConfig {
   local pre=${1:-""}
 
   ZDS=$ds debug "Looking for local configs (prefix: '$pre')"
-  if [[ -n $HOME/${pre}.zshrc.local(#qN) ]] &> /dev/null; then
-    ZDS=$ds debug "Found main local config, loading now"
-    source $HOME/${pre}.zshrc.local
+  local localConfig="$HOME/${pre}.zshrc.local"
+  if [[ -e localConfig ]] &> /dev/null; then
+    ZDS=$ds debug "Found main local config ($localConfig), loading now"
+    source $localConfig
   fi
-  if [[ -n $HOME/${pre}.zshrc.*.local(#qN) ]] &> /dev/null; then
+  local potentialLocalConfigs=$(ls -a $HOME | grep -E "^${pre}.zshrc" | grep -E ".local$")
+  if [[ ! -z $potentialLocalConfigs ]] &> /dev/null; then
     ZDS=$ds debug "Found local configs, loading now"
-    for localRc in $HOME/${pre}.zshrc.*.local; do
-      ZDS=$ds debug "Loading local config (at: $localRc)"
-      source $localRc
+    local localConfigs=($(echo $potentialLocalConfigs))
+    for localRc in $localConfigs; do
+      local localConfig="$HOME/$localRc"
+      ZDS=$ds debug "Loading local config (at: $localConfig)"
+      source $localConfig
     done
   fi
 }
